0dca1fe61e71d2640cf1b9331b03c54248275c28
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-03-30 12:30:55 -0600

    🎞️💲Movie Revenue Tree Map

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
         doping_plot: resolve(__dirname, 'doping_plot/index.html'),
         heat_map: resolve(__dirname, 'heat_map/index.html'),
         us_education: resolve(__dirname, 'us_education/index.html'),
+        tree_map: resolve(__dirname, 'tree_map/index.html')
       },
     },
   },
